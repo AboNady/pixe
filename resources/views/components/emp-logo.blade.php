c2218250f6aa0222f624d6136fab3f4437721cfe
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2025-12-18 17:35:06 +0200

    uppppp

--- resources/views/components/emp-logo.blade.php
+++ resources/views/components/emp-logo.blade.php
@@ -1,14 +1,7 @@
 @props(['logo', 'w' => 99])
 
-@php
-    // We removed 'file_exists' because scanning the disk is slow.
-    $logoPath = !empty($logo) 
-        ? asset('storage/'.$logo) 
-        : asset('images/default-company-logo.png'); // Replace with your actual default image
-@endphp
-
 <img 
-    src="{{ $logoPath }}"
+    src="{{ $logo }}"
     class="rounded-xl object-cover"
     style="width: {{ $w }}px; height: {{ $w }}px;"
     width="{{ $w }}" 
