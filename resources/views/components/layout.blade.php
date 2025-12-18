4a755f32a864759a72b4046bf3c60f72facd7582
Mahmoud Nady <111538752+AboNady@users.noreply.github.com>
2025-12-18 19:42:29 +0200

    Update and rename layoutt.blade.php to layout.blade.php commented

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pixel Positions</title>
     <link rel="icon" href="{{ asset('logo.svg') }}" type="image/svg+xml">
-
+<!-- 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500&display=swap" rel="stylesheet">
@@ -467,4 +467,4 @@
 
 </body>
 </html>
-
+ -->
